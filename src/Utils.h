976e326ef3ebad30150b4783676b6572fa722870
ksxjltze <leejiakeat@gmail.com>
2021-01-18 12:49:17 +0800

    comments

--- src/Utils.h
+++ src/Utils.h
@@ -5,6 +5,7 @@
 
 namespace StarBangBang
 {
+	//TODO make a better manager thing
 	AEVec2 GetMouseWorldPos();
 	AEVec2 GetGameObjectPos(GameObject* gameObject);
 	void SetGameObjectPos(GameObject* gameObject, AEVec2 newPos);
