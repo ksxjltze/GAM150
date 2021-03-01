a4bf2a54496f9aff0aa945bbd0a0bd40ed0220cd
ksxjltze <leejiakeat@gmail.com>
2021-03-01 17:55:39 +0800

    Tilemap load stub

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -1,9 +1,46 @@
 #include "TileMap.h"
 #include "constants.h"
 #include <fstream>
+#include "GraphicsManager.h"
 
-void StarBangBang::TileMap::Load()
+namespace StarBangBang
 {
-	std::ifstream is;
-	is.open(RESOURCES::LEVEL_TEST_PATH);
+	struct Tile
+	{
+		ImageComponent* spriteObject;
+	};
+
+	void StarBangBang::TileMap::Load(std::string path, ObjectManager& objMgr, GraphicsManager& gfxMgr)
+	{
+		std::ifstream is;
+		is.open(path);
+
+		if (is.is_open())
+		{
+			int y {0};
+
+			while (!is.eof())
+			{
+				int x {0};
+				std::string row;
+				is >> row;
+
+				//Test
+				Sprite sprite = gfxMgr.CreateSprite(RESOURCES::PROTOTYPE_SPRITE_GRASS_PATH);
+
+				for (auto ch : row)
+				{
+					GameObject* tileObj = objMgr.NewGameObject();
+					ImageComponent* spriteObj = objMgr.AddImage(tileObj, sprite);
+
+					Tile tile{ spriteObj };
+					map.insert({ {x++, y}, tile });
+				}
+				y++;
+
+			}
+			is.close();
+		}
+	}
+
 }
--- src/TileMap.h
+++ src/TileMap.h
@@ -1,10 +1,15 @@
 #pragma once
+#include <map>
+#include "ObjectManager.h"
 
 namespace StarBangBang
 {
+	struct Tile;
 	class TileMap
 	{
 	public:
-		void Load();
+		void Load(std::string path, ObjectManager& objMgr, GraphicsManager& gfxMgr);
+	private:
+		std::map<std::pair<int, int>, Tile> map;
 	};
 }
