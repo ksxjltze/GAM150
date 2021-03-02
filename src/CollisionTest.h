14f1ef504d21f8496985ec0af511fe7edebacc36
ksxjltze <leejiakeat@gmail.com>
2021-03-02 16:15:36 +0800

    Added check to prevent grid clash crash

--- src/CollisionTest.h
+++ src/CollisionTest.h
@@ -23,7 +23,8 @@ namespace StarBangBang
 		if (AEInputCheckTriggered(VK_RBUTTON))
 		{
 			A_Node* n = PathFinder::GetWorldGrid().GetNodeFromPosition(GetMouseWorldPos());
-			n->occupied = true;
+			if (n)
+				n->occupied = true;
 		}
 			
 		//set end pos and start pathfinding
