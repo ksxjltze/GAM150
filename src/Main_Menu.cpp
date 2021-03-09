e4b57a7300f29b9fa55051c2b857d34cdf364354
s-chinhin <simchinhin@gmail.com>
2021-03-09 14:48:54 +0800

    design abit of main menu and change spritesheet to imageby image

--- src/Main_Menu.cpp
+++ src/Main_Menu.cpp
@@ -16,39 +16,63 @@ StarBangBang::Main_Menu::Main_Menu(int id, GameStateManager& manager) : Scene(id
 void StarBangBang::Main_Menu::Load()
 {
 	//load button images
-	startgame_button = graphicsManager.CreateSprite(RESOURCES::STARTGAME_BUTTON_PATH);
-	settings_button = graphicsManager.CreateSprite(RESOURCES::SETTINGS_BUTTON_PATH);
-	credits_button = graphicsManager.CreateSprite(RESOURCES::CREDITS_BUTTON_PATH);
-	exitgame_button = graphicsManager.CreateSprite(RESOURCES::EXITGAME_BUTTON_PATH);
+	playbutton1 = graphicsManager.CreateSprite(RESOURCES::PLAY1_BUTTON_PATH);
+	settingsbutton1 = graphicsManager.CreateSprite(RESOURCES::SETTING1_BUTTON_PATH);
+	creditsbutton1 = graphicsManager.CreateSprite(RESOURCES::CREDIT1_BUTTON_PATH);
+	exitbutton1 = graphicsManager.CreateSprite(RESOURCES::EXIT1_BUTTON_PATH);
+
+	playbutton2 = graphicsManager.CreateSprite(RESOURCES::PLAY2_BUTTON_PATH);
+	settingsbutton2 = graphicsManager.CreateSprite(RESOURCES::SETTING2_BUTTON_PATH);
+	creditsbutton2 = graphicsManager.CreateSprite(RESOURCES::CREDIT2_BUTTON_PATH);
+	exitbutton2 = graphicsManager.CreateSprite(RESOURCES::EXIT2_BUTTON_PATH);
 }
 
 void StarBangBang::Main_Menu::Init()
 {
 	//start game button
-	startbutton_obj = objectManager.NewGameObject();
-	objectManager.AddImage(startbutton_obj, startgame_button);
-	startbutton_obj->transform.scale = { x_scale,y_scale };
-	startbutton_obj->transform.position = { 100,100 };
+	playbutton_obj = objectManager.NewGameObject();
+	objectManager.AddImage(playbutton_obj, playbutton1);
+	playbutton_obj->transform.position = { (float)AEGetWindowWidth()/-8, (float)AEGetWindowHeight() / 8 };
 
 	//settings button
 	settingsbutton_obj = objectManager.NewGameObject();
-	objectManager.AddImage(settingsbutton_obj, settings_button);
-	//settingsbutton_obj->transform.scale = { x_scale,y_scale };
+	objectManager.AddImage(settingsbutton_obj, settingsbutton1);
+	settingsbutton_obj->transform.position = { (float)AEGetWindowWidth() / 8, (float)AEGetWindowHeight() / 8 };
 
 	//credits button
 	creditsbutton_obj = objectManager.NewGameObject();
-	objectManager.AddImage(creditsbutton_obj, credits_button);
-	//creditsbutton_obj->transform.scale = { x_scale,y_scale };
+	objectManager.AddImage(creditsbutton_obj, creditsbutton1);
+	creditsbutton_obj->transform.position = { (float)AEGetWindowWidth() / -8, (float)AEGetWindowHeight() / -8 };
 
 	//exit game button
 	exitbutton_obj = objectManager.NewGameObject();
-	objectManager.AddImage(exitbutton_obj, exitgame_button);
-	//exitbutton_obj->transform.scale = { x_scale,y_scale };
+	objectManager.AddImage(exitbutton_obj, exitbutton1);
+	exitbutton_obj->transform.position = { (float)AEGetWindowWidth() / 8, (float)AEGetWindowHeight() / -8, };
 }
 
 void StarBangBang::Main_Menu::Update()
 {
 	Scene::Update();
+
+	if (tf == false)
+	{
+		objectManager.AddImage(settingsbutton_obj, settingsbutton1);
+	}
+	else
+	{
+		objectManager.AddImage(settingsbutton_obj, settingsbutton2);
+	}
+
+
+	if (AEInputCheckTriggered(VK_SPACE))
+	{
+		tf = true;
+	}
+
+	if (AEInputCheckTriggered(VK_F1))
+	{
+		tf = false;
+	}
 }
 
 void StarBangBang::Main_Menu::Draw()
@@ -63,7 +87,7 @@ void StarBangBang::Main_Menu::Free()
 
 void StarBangBang::Main_Menu::Unload()
 {
-	Scene::Free();
+	Scene::Unload();
 }
 
 
--- src/constants.cpp
+++ src/constants.cpp
@@ -11,19 +11,95 @@ namespace StarBangBang
 	}
 	namespace RESOURCES
 	{
+		//Folder paths
 		const std::string&& ROOT_PATH					= "../Resources/";
 		const std::string&& LEVELS_PATH					= ROOT_PATH + "Levels/";
-		const std::string&& MAINMENU_PATH				= "MainMenu/";
-		const std::string&& PROTOTYPE_SPRITE_1_PATH		= ROOT_PATH + "PlanetTexture.png";
-		const std::string&& PROTOTYPE_SPRITE_2_PATH		= ROOT_PATH + "boi.png";
+		const std::string&& MAINMENU_PATH				= ROOT_PATH + "MainMenu/";
+		const std::string&& CHARACTERS_PATH				= ROOT_PATH + "Characters/";
+		const std::string&& FLOORTILES_PATH				= ROOT_PATH + "FloorTiles/";
+		const std::string&& LEVEL_TEST_PATH				= LEVELS_PATH + "test.txt";
+		
+		//Images path
+		const std::string&& PROTOTYPE_SPRITE_1_PATH = ROOT_PATH + "PlanetTexture.png";
+		const std::string&& PROTOTYPE_SPRITE_2_PATH = ROOT_PATH + "boi.png";
 		const std::string&& PROTOTYPE_SPRITE_GRASS_PATH = ROOT_PATH + "grass.png";
 		const std::string&& PROTOTYPE_SPRITE_STONE_PATH = ROOT_PATH + "stone.png";
-		const std::string&& SPRITE_PLAYER_PATH			= ROOT_PATH + "player.png";
-						  
-		const std::string&& STARTGAME_BUTTON_PATH		= ROOT_PATH + MAINMENU_PATH + "StartGameButton.png";
-		const std::string&& SETTINGS_BUTTON_PATH		= ROOT_PATH + MAINMENU_PATH + "SettingsButton.png";
-		const std::string&& CREDITS_BUTTON_PATH			= ROOT_PATH + MAINMENU_PATH + "CreditsButton.png";
-		const std::string&& EXITGAME_BUTTON_PATH		= ROOT_PATH + MAINMENU_PATH + "ExitGameButton.png";
-		const std::string&& LEVEL_TEST_PATH				= LEVELS_PATH + "test.txt";
+		const std::string&& SPRITE_PLAYER_PATH = ROOT_PATH + "player.png";
+
+		//main menu buttons sprites
+		const std::string&& PLAY1_BUTTON_PATH			= MAINMENU_PATH + "Play1.png";
+		const std::string&& PLAY2_BUTTON_PATH			= MAINMENU_PATH + "Play2.png";
+		const std::string&& SETTING1_BUTTON_PATH		= MAINMENU_PATH + "Setting1.png";
+		const std::string&& SETTING2_BUTTON_PATH		= MAINMENU_PATH + "Setting2.png";
+		const std::string&& CREDIT1_BUTTON_PATH			= MAINMENU_PATH + "Credit1.png";
+		const std::string&& CREDIT2_BUTTON_PATH			= MAINMENU_PATH + "Credit2.png";
+		const std::string&& EXIT1_BUTTON_PATH			= MAINMENU_PATH + "Exit1.png";
+		const std::string&& EXIT2_BUTTON_PATH			= MAINMENU_PATH + "Exit2.png";
+
+		//Character sprites
+		const std::string&& CAPTAINSTEALTH_F1_PATH			= CHARACTERS_PATH + "CaptainStealthForward1.png";
+		const std::string&& CAPTAINSTEALTH_F2_PATH			= CHARACTERS_PATH + "CaptainStealthForward2.png";
+		const std::string&& CAPTAINSTEALTH_F3_PATH			= CHARACTERS_PATH + "CaptainStealthForward3.png";
+
+		const std::string&& CAPTAINSTEALTH_B1_PATH			= CHARACTERS_PATH + "CaptainStealthBack1.png";
+		const std::string&& CAPTAINSTEALTH_B2_PATH			= CHARACTERS_PATH + "CaptainStealthBack2.png";
+		const std::string&& CAPTAINSTEALTH_B3_PATH			= CHARACTERS_PATH + "CaptainStealthBack3.png";
+
+		const std::string&& CAPTAINSTEALTH_L1_PATH			= CHARACTERS_PATH + "CaptainStealthLeft1.png";
+		const std::string&& CAPTAINSTEALTH_L2_PATH			= CHARACTERS_PATH + "CaptainStealthLeft2.png";
+		const std::string&& CAPTAINSTEALTH_L3_PATH			= CHARACTERS_PATH + "CaptainStealthLeft3.png";
+
+		const std::string&& CAPTAINSTEALTH_R1_PATH			= CHARACTERS_PATH + "CaptainStealthRight1.png";
+		const std::string&& CAPTAINSTEALTH_R2_PATH			= CHARACTERS_PATH + "CaptainStealthRight2.png";
+		const std::string&& CAPTAINSTEALTH_R3_PATH			= CHARACTERS_PATH + "CaptainStealthRight3.png";
+
+		const std::string&& PRISONER_F1_PATH				= CHARACTERS_PATH + "PrisonerForward1.png";
+		const std::string&& PRISONER_F2_PATH				= CHARACTERS_PATH + "PrisonerForward2.png";
+		const std::string&& PRISONER_F3_PATH				= CHARACTERS_PATH + "PrisonerForward3.png";
+
+		const std::string&& PRISONER_B1_PATH				= CHARACTERS_PATH + "PrisonerBack1.png";
+		const std::string&& PRISONER_B2_PATH				= CHARACTERS_PATH + "PrisonerBack2.png";
+		const std::string&& PRISONER_B3_PATH				= CHARACTERS_PATH + "PrisonerBack3.png";
+
+		const std::string&& PRISONER_L1_PATH				= CHARACTERS_PATH + "PrisonerLeft1.png";
+		const std::string&& PRISONER_L2_PATH				= CHARACTERS_PATH + "PrisonerLeft2.png";
+		const std::string&& PRISONER_L3_PATH				= CHARACTERS_PATH + "PrisonerLeft3.png";
+
+		const std::string&& PRISONER_R1_PATH				= CHARACTERS_PATH + "PrisonerRight1.png";
+		const std::string&& PRISONER_R2_PATH				= CHARACTERS_PATH + "PrisonerRight2.png";
+		const std::string&& PRISONER_R3_PATH				= CHARACTERS_PATH + "PrisonerRight3.png";
+
+		const std::string&& SECURITYGUARD_F1_PATH			= CHARACTERS_PATH + "SecurityGuardForward1.png";
+		const std::string&& SECURITYGUARD_F2_PATH			= CHARACTERS_PATH + "SecurityGuardForward2.png";
+		const std::string&& SECURITYGUARD_F3_PATH			= CHARACTERS_PATH + "SecurityGuardForward3.png";
+
+		const std::string&& SECURITYGUARD_B1_PATH			= CHARACTERS_PATH + "SecurityGuardBack1.png";
+		const std::string&& SECURITYGUARD_B2_PATH			= CHARACTERS_PATH + "SecurityGuardBack2.png";
+		const std::string&& SECURITYGUARD_B3_PATH			= CHARACTERS_PATH + "SecurityGuardBack3.png";
+
+		const std::string&& SECURITYGUARD_L1_PATH			= CHARACTERS_PATH + "SecurityGuardLeft1.png";
+		const std::string&& SECURITYGUARD_L2_PATH			= CHARACTERS_PATH + "SecurityGuardLeft2.png";
+		const std::string&& SECURITYGUARD_L3_PATH			= CHARACTERS_PATH + "SecurityGuardLeft3.png";
+
+		const std::string&& SECURITYGUARD_R1_PATH			= CHARACTERS_PATH + "SecurityGuardRight1.png";
+		const std::string&& SECURITYGUARD_R2_PATH			= CHARACTERS_PATH + "SecurityGuardRight2.png";
+		const std::string&& SECURITYGUARD_R3_PATH			= CHARACTERS_PATH + "SecurityGuardRight3.png";
+
+		//floortiles sprites
+		const std::string&& BLACKBRICK_PATH					= FLOORTILES_PATH + "BlackBrick.png";
+		const std::string&& BLACKBRICK_UP_PATH				= FLOORTILES_PATH + "BlackBrickUp.png";
+		const std::string&& BLACKBRICK_DOWN_PATH			= FLOORTILES_PATH + "BlackBrickDown.png";
+		const std::string&& BLACKBRICK_LEFT_PATH			= FLOORTILES_PATH + "BlackBrickLeft.png";
+		const std::string&& BLACKBRICK_RIGHT_PATH			= FLOORTILES_PATH + "BlackBrickRight.png";
+
+
+		const std::string&& REDBRICK_PATH					= FLOORTILES_PATH + "RedBrick.png";
+		const std::string&& REDBRICK_UP_PATH				= FLOORTILES_PATH + "RedBrickUp.png";
+		const std::string&& REDBRICK_DOWN_PATH				= FLOORTILES_PATH + "RedBrickDown.png";
+		const std::string&& REDBRICK_LEFT_PATH				= FLOORTILES_PATH + "RedBrickLeft.png";
+		const std::string&& REDBRICK_RIGHT_PATH				= FLOORTILES_PATH + "RedBrickRight.png";
+		
+		const std::string&& OFFICEFLOOR_PATH				= FLOORTILES_PATH + "OfficeFloor.png";
+		const std::string&& PRISONFLOOR_PATH				= FLOORTILES_PATH + "PrisonFloor.png";
 	}
 }
