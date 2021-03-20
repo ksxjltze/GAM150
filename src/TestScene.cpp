2eeae8e35966407759bc59d1ba972b402bda301b
Lee Jia Keat <leejiakeat@gmail.com>
2021-03-21 03:27:19 +0800

    Event examples using DetectionListener and ListenerComponent (TestScene)

--- src/TestScene.cpp
+++ src/TestScene.cpp
@@ -1,6 +1,7 @@
 #include "TestScene.h"
 #include "TestInteractable.h"
 #include "TestListener.h"
+#include "DetectionListener.h"
 
 namespace StarBangBang
 {
@@ -25,6 +26,35 @@ void StarBangBang::TestScene::Init()
 {
 	//Add Listener to event system
 	MessageBus::RegisterListener(&Test::listener);
+
+	//Create new game object to listen for events
+	GameObject* listenerObj = objectManager.NewGameObject();
+
+	//Attach Listener Component
+	objectManager.AddComponent<DetectionListener>(listenerObj);
+
+	//Get Listener Component
+	DetectionListener* detectionListener = listenerObj->GetComponent<DetectionListener>();
+
+	//DetectionListener Component inherits from ListenerComponent
+	//Polymorphism allows the following line
+	ListenerComponent* listenerComponent = detectionListener;
+
+	//Listener Component inherits from Listener
+	//Polymorphism allows the following line
+	Listener* listenPtr = listenerComponent;
+
+	//The following code registers the same listener 3 times
+	//As a result, any events that are received will also notify the same listener 3 times.
+	
+	//Add Listener to event system
+	MessageBus::RegisterListener(listenPtr);
+
+	//Alternative
+	MessageBus::RegisterListener(listenerComponent);
+
+	//Alternative 2
+	MessageBus::RegisterListener(detectionListener);
 }
 
 void StarBangBang::TestScene::Update()
@@ -33,6 +63,16 @@ void StarBangBang::TestScene::Update()
 	if (AEInputCheckTriggered(AEVK_T))
 	{
 		Test::interactableObject.Interact();
+	}
+
+	//Send DETECTION Message on key press (E)
+	if (AEInputCheckTriggered(AEVK_E))
+	{
+		Event detectionEvent;
+		detectionEvent.id = EventId::DETECTED;
+		detectionEvent.context = std::string("\nTESTING DETECTION LISTENER\n");
+
+		MessageBus::Notify(detectionEvent);
 	}
 }
 
@@ -43,10 +83,10 @@ void StarBangBang::TestScene::Draw()
 
 void StarBangBang::TestScene::Free()
 {
-
+	Scene::Free();
 }
 
 void StarBangBang::TestScene::Unload()
 {
-
+	Scene::Unload();
 }
