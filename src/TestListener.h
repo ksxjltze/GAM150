f88327819e4d3816171ad3dd7426afc6e0ffd898
Lee Jia Keat <leejiakeat@gmail.com>
2021-03-21 03:15:50 +0800

    Added some documentation to DetectionListener

--- src/TestListener.h
+++ src/TestListener.h
@@ -6,6 +6,7 @@ namespace StarBangBang
 	/// <summary>
 	/// Inherit (publicly) from Listener abstract class (interface) and implement onNotify()
 	/// MessageBus (Event system) will call onNotify when a new message is received
+	/// See DetectionListener.h for an example using ListenerComponent instead.
 	/// </summary>
 	class TestListener : public Listener
 	{
