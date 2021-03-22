70ff3510abf9bbf881801d2ee72c56cec3ea613a
raynerlrh <raynerlrh@gmail.com>
2021-03-22 23:31:44 +0800

    Removed printing mouse pos

--- src/GuardManager.cpp
+++ src/GuardManager.cpp
@@ -83,7 +83,7 @@ void GuardManager::CreateSecurityCameras(ObjectManager* objManager, Sprite* spri
 
 void GuardManager::Update()
 {
-	PRINT("x: %f, y: %f\n", GetMouseWorldPos().x, GetMouseWorldPos().y);
+	//PRINT("x: %f, y: %f\n", GetMouseWorldPos().x, GetMouseWorldPos().y);
 
 	// upon receiving distraction event, get nearest guard to be distracted
 	// ...
