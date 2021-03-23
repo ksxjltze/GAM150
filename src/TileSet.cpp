9cb3d2bafe326b598f4c3fb1de627fcc07130533
Lee Jia Keat <leejiakeat@gmail.com>
2021-03-23 09:22:00 +0800

    change floor

--- src/TileSet.cpp
+++ src/TileSet.cpp
@@ -9,7 +9,8 @@ namespace StarBangBang
 		LoadTileSprite(gfxMgr,	TileType::GRASS,			RESOURCES::PROTOTYPE_SPRITE_GRASS_PATH);
 		LoadTileSprite(gfxMgr,	TileType::STONE,			RESOURCES::PROTOTYPE_SPRITE_STONE_PATH);
 		LoadTileSprite(gfxMgr,	TileType::BRICK_BLACK,		RESOURCES::BLACKBRICK_PATH);
-		LoadTileSprite(gfxMgr,	TileType::BRICK_RED,		RESOURCES::REDBRICK_PATH);
+		LoadTileSprite(gfxMgr,	TileType::BRICK_RED,		RESOURCES::PRISONFLOOR_PATH);
+		//LoadTileSprite(gfxMgr,	TileType::BRICK_RED,		RESOURCES::REDBRICK_PATH);
 		LoadTileSprite(gfxMgr,	TileType::FLOOR_OFFICE,		RESOURCES::OFFICEFLOOR_PATH);
 		LoadTileSprite(gfxMgr,	TileType::FLOOR_PRISON,		RESOURCES::PRISONFLOOR_PATH);
 		LoadTileSprite(gfxMgr,	TileType::TEST,				RESOURCES::SPRITE_PLAYER_PATH);
