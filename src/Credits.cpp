f53c2f4c3febcb623003980ac8fcf8156cc7c1c0
Lee Jia Keat <leejiakeat@gmail.com>
2021-03-30 09:51:16 +0800

    Credits scene

---
+++ src/Credits.cpp
@@ -0,0 +1,36 @@
+#include "Credits.h"
+
+StarBangBang::Credits::Credits(int id, GameStateManager& gsm) : Scene(id, gsm)
+{
+
+}
+
+void StarBangBang::Credits::Load()
+{
+
+}
+
+void StarBangBang::Credits::Init()
+{
+
+}
+
+void StarBangBang::Credits::Update()
+{
+	Scene::Update();
+}
+
+void StarBangBang::Credits::Draw()
+{
+	Scene::Draw();
+}
+
+void StarBangBang::Credits::Free()
+{
+	Scene::Free();
+}
+
+void StarBangBang::Credits::Unload()
+{
+	Scene::Unload();
+}
---
+++ src/Credits.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "State.h"
+
+namespace StarBangBang
+{
+	class Credits : public Scene
+	{
+	public:
+		Credits(int id, GameStateManager& gsm);
+		void Load();
+		void Init();
+		void Update();
+		void Draw();
+		void Free();
+		void Unload();
+	};
+
+}
