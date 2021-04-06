e4c73b25ba4524106c9b2aded38a91ce855dc07c
s-chinhin <simchinhin@gmail.com>
2021-04-06 14:37:46 +0800

    vent

--- src/Level_Demo.cpp
+++ src/Level_Demo.cpp
@@ -275,13 +275,32 @@ namespace StarBangBang
 		CreateDistraction(roomNum, 16, 27, vendingMachineSprite);
 		CreateDistraction(roomNum, 5, 47, vendingMachineSprite);
 
+		//room1
 		CreateVent(11, 12);
 		CreateVent(18, 7);
+		
+		//room 2
 		CreateVent(30, 10);
-		CreateVent(30, 16);
-		CreateVent(35, 13);
+		//CreateVent(30, 16);
+		//CreateVent(35, 13);
 		CreateVent(45, 13);
 		CreateVent(42, 4);
+
+		//room 3
+		CreateVent(21, 26);
+		CreateVent(40, 24);
+		CreateVent(45, 39);
+		CreateVent(35, 36);
+		CreateVent(26, 43);
+		//CreateVent(41, 43);
+
+		//room 4
+		CreateVent(15, 41);
+		CreateVent(15, 33);
+		CreateVent(8, 20);
+		//CreateVent(7, 37);
+		CreateVent(7, 45);
+		//CreateVent(6, 26);
 
 
 		//Notification Text
