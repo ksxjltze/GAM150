18a0aa22e0d7b232155d260285a0a7b86f37a6e5
Lee Jia Keat <leejiakeat@gmail.com>
2021-04-10 19:30:09 +0800

    cleanup

--- src/Movement.h
+++ src/Movement.h
@@ -1,3 +1,19 @@
+/******************************************************************************/
+/*!
+\title		Captain Stealth
+\file		Movement.h
+\author 	Lee Jia Keat
+\par    	email: l.jiakeat\@digipen.edu
+\date   	April 09, 2021
+\brief		Movement script.
+			Directly translates the game object using WASD.
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+*/
+/******************************************************************************/
+
 #pragma once
 #include "ScriptComponent.h"
 
