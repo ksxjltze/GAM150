725f9213359a8bf5dac8cf575fd86ce2fe672b03
Lee Jia Keat <leejiakeat@gmail.com>
2021-04-10 19:27:42 +0800

    d

--- src/Click.h
+++ src/Click.h
@@ -1,3 +1,19 @@
+/******************************************************************************/
+/*!
+\title		Captain Stealth
+\file		Click.h
+\author 	Lee Jia Keat
+\par    	email: l.jiakeat\@digipen.edu
+\date   	April 09, 2021
+\brief		Click script.
+			Invokes the set callback when the game object is clicked.
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+*/
+/******************************************************************************/
+
 #pragma once
 #include "ScriptComponent.h"
 #include "Utils.h"
