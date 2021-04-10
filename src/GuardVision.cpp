a7e0ef2724db27297a51a83087960ed6819dfb9a
raynerlrh <raynerlrh@gmail.com>
2021-04-11 01:34:26 +0800

    Guard rotation thing

--- src/GuardVision.cpp
+++ src/GuardVision.cpp
@@ -74,12 +74,9 @@ void GuardVision::Update()
 		float dpResult = AEVec2DotProduct(&defaultForward, &targetDir);
 		float targetRot = AERadToDeg(AEACos(dpResult));
 
-		if (gameObject->GetComponent<Guard>()->GetState() == Guard::GUARD_STATE::STATE_PATROL)
-		{
-			float dp = AEVec2DotProduct(&defaultLeft, &targetDir);
-			if (dp < 0.f)
-				targetRot = -targetRot;
-		}
+		float dp = AEVec2DotProduct(&defaultLeft, &targetDir);
+		if (dp < 0.f)
+			targetRot = -targetRot;
 
 		currRot = static_cast<int>(rint(targetRot));
 
