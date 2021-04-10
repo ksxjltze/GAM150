8ad37b0ee8f6bfa8b8bef6d8b6c0c7a8cabc2f8d
raynerlrh <raynerlrh@gmail.com>
2021-04-10 20:34:16 +0800

    Documentation (4)

--- src/CaughtByCameraEvent.h
+++ src/CaughtByCameraEvent.h
@@ -1,3 +1,19 @@
+/*!*********************************************************************
+\title	  Captain Stealth
+\file     CaughtByCameraEvent.h
+\author   Liew Ruiheng Rayner (100%)
+\par      DP email: r.liew\@digipen.edu
+\date     10/04/2021
+
+\brief
+		  Caught by camera event. Contains data such as the room number
+		  that the event took place in.
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+***********************************************************************/
+
 #pragma once
 #include "GameObject.h"
 #include "MessageBus.h"
@@ -11,6 +27,10 @@ namespace StarBangBang
 		AEVec2 pos;
 		unsigned int roomNum = 0;
 
+		/*!*********************************************************************
+		\brief
+			Sends event to the message bus for listeners to respond
+		***********************************************************************/
 		void SendEvent()
 		{
 			roomNum = gameObject->GetComponent<SecurityCamera>()->GetRoomNum();
@@ -19,6 +39,5 @@ namespace StarBangBang
 			e.context = *this;
 			MessageBus::Notify(e);
 		};
-
 	};
 }
