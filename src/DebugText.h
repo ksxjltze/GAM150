2bbe7fc5e0c53896a0d8379728877124f21b2736
Lee Jia Keat <leejiakeat@gmail.com>
2021-04-11 14:41:31 +0800

    DebugText documentation

--- src/DebugText.h
+++ src/DebugText.h
@@ -24,11 +24,46 @@ namespace StarBangBang
 	class DebugText : public ListenerComponent
 	{
 	public:
+		/*!*************************************************************************
+		 * \brief Component Constructor.
+		 * 
+		 * \param obj GameObject to attach to.
+		 * \param fontId Font to use for text rendering.
+		***************************************************************************/
 		DebugText(GameObject* obj, s8 fontId = -1);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Sets the font to use for text rendering.
+		 * \param fontId FontId to use for text rendering.
+		***************************************************************************/
 		void SetFont(s8 fontId);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Sets the textbox dimensions.
+		 * \param width Textbox Width
+		 * \param height Textbox Height
+		***************************************************************************/
 		void SetTextbox(float width, float height);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Listener callback. Invoked when an event is received from the Message Bus.
+		 * \param e Event data.
+		***************************************************************************/
 		void onNotify(Event e);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Update function. Called once per frame.
+		***************************************************************************/
 		void Update();
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Draw function. Called once per frame.
+		***************************************************************************/
 		void Draw();
 	private:
 		float duration;
