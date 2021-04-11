f029a6bda68f466579e192c510549512d7e0e032
Lee Jia Keat <leejiakeat@gmail.com>
2021-04-11 15:05:20 +0800

    ScriptComponent Documentation

--- src/ScriptComponent.h
+++ src/ScriptComponent.h
@@ -24,11 +24,39 @@ namespace StarBangBang
 	class Script : public Component<Script>
 	{
 	public:
+		/*!*************************************************************************
+		 * \brief 
+		 * Component Constructor.
+		 * Attaches the component to a game object.
+		 * \param gameObject
+		 * GameObject to attach to.
+		***************************************************************************/
 		inline Script(GameObject* gameObject) : Component(gameObject) {}
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Sets the status of the script.
+		 * \param status
+		 * True for active, false otherwise
+		***************************************************************************/
 		inline void SetActive(bool status) { active = status; }
 
+		/*!*************************************************************************
+		 * \brief 
+		 * Start function. Called once at the start of the scene.
+		***************************************************************************/
 		virtual void Start() {};
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Update function. Called once per frame.
+		***************************************************************************/
 		virtual void Update() {};
+
+		/*!*************************************************************************
+		 * \brief
+		 * Draw function. Called once per frame.
+		***************************************************************************/
 		virtual void Draw() {};
 
 	private:
