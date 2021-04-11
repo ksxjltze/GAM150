b4bfbaf551e21dc5bc95d1612028815a32e50adb
Lee Jia Keat <leejiakeat@gmail.com>
2021-04-12 02:26:30 +0800

    d

--- src/CollisionEvent.h
+++ src/CollisionEvent.h
@@ -1,3 +1,20 @@
+/******************************************************************************/
+/*!
+\title		Captain Stealth
+\file		DistractionEvent.h
+\author 	Lee Jia Keat
+\par    	email: l.jiakeat\@digipen.edu
+\date   	April 09, 2021
+\brief		Distraction Event.
+			Contains data for distraction, such as the duration of the distraction,
+			its room number and the game object that is doing the distracting.
+
+Copyright (C) 2021 DigiPen Institute of Technology Singapore. All rights reserved.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+*/
+/******************************************************************************/
+
 #pragma once
 #include "GameObject.h"
 #include "Collider.h"
@@ -10,6 +27,12 @@ namespace StarBangBang
 		Collider* first;
 		Collider* second;
 
+		/*!*************************************************************************
+		 * \brief 
+		 * Sends a collision event to the message bus.
+		 * \return
+		 * void
+		***************************************************************************/
 		void SendEvent() 
 		{ 
 			Event e;
--- src/DistractionEvent.h
+++ src/DistractionEvent.h
@@ -1,3 +1,21 @@
+/******************************************************************************/
+/*!
+\title		Captain Stealth
+\file		DistractionEvent.h
+\author 	Lee Jia Keat
+\par    	email: l.jiakeat\@digipen.edu
+\date   	April 09, 2021
+\brief		Distraction Event.
+			Contains data for distraction, such as the duration of the distraction,
+			its room number and the game object that is doing the distracting.
+
+Copyright (C) 2021 DigiPen Institute of Technology Singapore. All rights reserved.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+*/
+/******************************************************************************/
+
+
 #pragma once
 #include "GameObject.h"
 #include "MessageBus.h"
@@ -11,6 +29,12 @@ namespace StarBangBang
 		unsigned int roomNum = 0;
 		float duration;
 
+		/*!*************************************************************************
+		 * \brief 
+		 * Sends a distraction event to the message bus.
+		 * \return
+		 * void
+		***************************************************************************/
 		void SendEvent()
 		{
 			roomNum = gameObject->GetComponent<Distractor>()->GetRoomNum();
