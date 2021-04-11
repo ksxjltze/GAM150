090ae8896f37c20e8dd479b5118c49e07759990f
Lee Jia Keat <leejiakeat@gmail.com>
2021-04-11 14:51:08 +0800

    AudioEngine doc

--- src/AudioEngine.h
+++ src/AudioEngine.h
@@ -37,17 +37,107 @@ namespace StarBangBang
 	class AudioEngine : public Listener
 	{
 	public:
+		/*!*************************************************************************
+		 * \brief 
+		 * Audio Engine constructor. 
+		 * Initializes the fmod Audio System and creates the channel groups for use.
+		***************************************************************************/
 		AudioEngine();
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Creates a sound from the provided file path.
+		 * \param sound 
+		 * Output Pointer to a Pointer of the sound object.
+		 * \param file 
+		 * C style string to the file path of the sound.
+		***************************************************************************/
 		void CreateSound(FMOD::Sound** sound, const char* file);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Creates a sound from the provided file path..
+		 * \param sound
+		 * Output Pointer to a Pointer of the sound object.
+		 * \param 
+		 * string to the file path of the sound.
+		***************************************************************************/
 		void CreateSound(FMOD::Sound** sound, const std::string&);
+
+		/*!*************************************************************************
+		 * \brief
+		 * Listener callback. Invoked when an event is received from the Message Bus.
+		 * Listens for events: PLAY_SOUND, PLAY_MUSIC, STOP_SOUND and MUTE
+		 * \param e Event data.
+		***************************************************************************/
 		void onNotify(Event e);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Maps a sound object to a specified name.
+		 * \param name
+		 * Name to map to.
+		 * \param sound
+		 * Pointer to the sound object to map.
+		***************************************************************************/
 		void AddSound(const std::string& name, FMOD::Sound* sound);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Plays a sound.
+		 * \param sound
+		 * Pointer to the sound object to play.
+		 * \param loop
+		 * True to loop, false to play once
+		 * \param cgId
+		 * Channel Group Id
+		***************************************************************************/
 		void playSound(FMOD::Sound* sound, bool loop = false, ChannelGroupId cgId = ChannelGroupId::SFX);
+
+		/*!*************************************************************************
+		 * \brief
+		 * Plays a sound by name.
+		 * \param name
+		 * Name of the sound object.
+		 * \param loop
+		 * True to loop, false to play once
+		 * \param cgId
+		 * Channel Group Id
+		***************************************************************************/
 		void playSound(const std::string& name, bool loop = false, ChannelGroupId cgId = ChannelGroupId::SFX);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Releases a sound object, freeing the allocated memory.
+		 * \param sound
+		 * Pointer to the sound object to free.
+		***************************************************************************/
 		void ReleaseSound(FMOD::Sound* sound);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Stops the master channel.
+		***************************************************************************/
 		void StopMasterChannel();
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Update function. Called once per frame.
+		***************************************************************************/
 		void Update();
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Mutes a channel group.
+		 * \param cgId
+		 * Channel Group Id
+		***************************************************************************/
 		void Mute(ChannelGroupId cgId);
+
+		/*!*************************************************************************
+		 * \brief 
+		 * Closes the sound system.
+		***************************************************************************/
 		void Exit();
 	private:
 		bool paused{ false };
